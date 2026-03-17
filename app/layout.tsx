0745328f7d35bd1d149288f1350571ec8110fed5
jhollen <jerod.a.hollen@gmail.com>
2026-03-17 09:33:04 -0700

    feat: add vercel analytics to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+});
 
 // Import Next.js Metadata type for SEO and page info
 export const metadata: Metadata = {
@@ -13,21 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        {/* Import Roboto from Google Fonts */}
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-        />
-        <style>
-          {`
-            body {
-              font-family: 'Roboto', sans-serif;
-            }
-          `}
-        </style>
-      </head>
+    <html lang="en" className={roboto.className}>
+      <head></head>
       <body className="bg-[#121212] m-0 p-0 text-white antialiased">
         {children}
       </body>
